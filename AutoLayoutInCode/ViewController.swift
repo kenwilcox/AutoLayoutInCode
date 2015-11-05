c3be61708c93b3d1982436a44ef6cc84ae274453
Ken Wilcox <wilcoxk@gmail.com>
2015-11-04 22:29:14 -0700

    Cheesy code

--- AutoLayoutInCode/ViewController.swift
+++ AutoLayoutInCode/ViewController.swift
@@ -13,6 +13,37 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let label1 = UILabel()
+    label1.translatesAutoresizingMaskIntoConstraints = false
+    label1.backgroundColor = UIColor.redColor()
+    label1.text = "THESE"
+    
+    let label2 = UILabel()
+    label2.translatesAutoresizingMaskIntoConstraints = false
+    label2.backgroundColor = UIColor.cyanColor()
+    label2.text = "ARE"
+    
+    let label3 = UILabel()
+    label3.translatesAutoresizingMaskIntoConstraints = false
+    label3.backgroundColor = UIColor.yellowColor()
+    label3.text = "SOME"
+    
+    let label4 = UILabel()
+    label4.translatesAutoresizingMaskIntoConstraints = false
+    label4.backgroundColor = UIColor.greenColor()
+    label4.text = "AWESOME"
+    
+    let label5 = UILabel()
+    label5.translatesAutoresizingMaskIntoConstraints = false
+    label5.backgroundColor = UIColor.orangeColor()
+    label5.text = "LABELS"
+    
+    view.addSubview(label1)
+    view.addSubview(label2)
+    view.addSubview(label3)
+    view.addSubview(label4)
+    view.addSubview(label5)
   }
   
   override func didReceiveMemoryWarning() {
@@ -20,6 +51,8 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
+  override func prefersStatusBarHidden() -> Bool {
+    return true
+  }
 }
 
